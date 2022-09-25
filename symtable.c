4e8fcf1ce6fab31a3e49a4ef891a280db486e290
Josef Kuchař <josef.kuchar267@gmail.com>
2022-09-25 20:27:24 +0200

    Fix build

--- symtable.c
+++ symtable.c
@@ -5,6 +5,69 @@
 #define AVG_LEN_MIN 0.5
 #define AVG_LEN_MAX 2.0
 
+size_t htab_hash_function(const char* str) {
+    uint32_t h = 0;  // musí mít 32 bitů
+    const unsigned char* p;
+    for (p = (const unsigned char*)str; *p != '\0'; p++)
+        h = 65599 * h + *p;
+    return h;
+}
+
+void htab_resize(htab_t* t, size_t newn) {
+    if (t == NULL) {
+        return;
+    }
+
+    // Allocate new array
+    struct htab_item** new_arr = malloc(newn * sizeof(struct htab_item*));
+    if (new_arr == NULL) {
+        return;  // Nothing happens
+    }
+
+    // Initialize new array
+    for (size_t i = 0; i < newn; i++) {
+        new_arr[i] = NULL;
+    }
+
+    // Rehash old items into new array
+    for (size_t i = 0; i < t->arr_size; i++) {
+        struct htab_item* item = t->arr_ptr[i];
+        while (item != NULL) {
+            // Get reference to next item
+            struct htab_item* next = item->next;
+
+            // Reset item's next pointer
+            item->next = NULL;
+
+            // Find new bucket
+            size_t index = htab_hash_function(item->pair.key) % newn;
+
+            struct htab_item* current = new_arr[index];
+
+            // Add to head
+            if (current == NULL) {
+                new_arr[index] = item;
+            } else {
+                // Add to tail
+                while (current->next != NULL) {
+                    current = current->next;
+                }
+                current->next = item;
+            }
+
+            // Update pointer to next item
+            item = next;
+        }
+    }
+
+    // Free old array
+    free(t->arr_ptr);
+
+    // Update array pointer and size inside table
+    t->arr_ptr = new_arr;
+    t->arr_size = newn;
+}
+
 size_t htab_bucket_count(const htab_t* t) {
     if (t == NULL) {
         return 0;
@@ -140,14 +203,6 @@ void htab_free(htab_t* t) {
     free(t);
 }
 
-size_t htab_hash_function(const char* str) {
-    uint32_t h = 0;  // musí mít 32 bitů
-    const unsigned char* p;
-    for (p = (const unsigned char*)str; *p != '\0'; p++)
-        h = 65599 * h + *p;
-    return h;
-}
-
 htab_t* htab_init(size_t n) {
     // Allocate memory for the table
     htab_t* t = malloc(sizeof(htab_t));
@@ -226,61 +281,6 @@ htab_pair_t* htab_lookup_add(htab_t* t, htab_key_t key) {
 
     // Return new item
     return &(item->pair);
-}
-
-void htab_resize(htab_t* t, size_t newn) {
-    if (t == NULL) {
-        return;
-    }
-
-    // Allocate new array
-    struct htab_item** new_arr = malloc(newn * sizeof(struct htab_item*));
-    if (new_arr == NULL) {
-        return;  // Nothing happens
-    }
-
-    // Initialize new array
-    for (size_t i = 0; i < newn; i++) {
-        new_arr[i] = NULL;
-    }
-
-    // Rehash old items into new array
-    for (size_t i = 0; i < t->arr_size; i++) {
-        struct htab_item* item = t->arr_ptr[i];
-        while (item != NULL) {
-            // Get reference to next item
-            struct htab_item* next = item->next;
-
-            // Reset item's next pointer
-            item->next = NULL;
-
-            // Find new bucket
-            size_t index = htab_hash_function(item->pair.key) % newn;
-
-            struct htab_item* current = new_arr[index];
-
-            // Add to head
-            if (current == NULL) {
-                new_arr[index] = item;
-            } else {
-                // Add to tail
-                while (current->next != NULL) {
-                    current = current->next;
-                }
-                current->next = item;
-            }
-
-            // Update pointer to next item
-            item = next;
-        }
-    }
-
-    // Free old array
-    free(t->arr_ptr);
-
-    // Update array pointer and size inside table
-    t->arr_ptr = new_arr;
-    t->arr_size = newn;
 }
 
 size_t htab_size(const htab_t* t) {
