4ca803ad619693332a45e44c2478160e57a2cb7c
S1ro1 <matej.sirovatka@gmail.com>
2022-10-03 13:01:57 +0200

    Reordering of precendece table and token_type enum to match it

--- token.h
+++ token.h
@@ -7,30 +7,30 @@
  * @brief All token types
  */
 typedef enum {
-    TOK_EOF,        // End of file
-    TOK_VAR,        // Identifier (eg. $foo)
-    TOK_STR_LIT,    // String literal (eg. "foo")
-    TOK_INT_LIT,    // Integer literal (eg. 0)
-    TOK_FLOAT_LIT,  // Float literal (eg. 0.0)
-    TOK_BOOL_LIT,   // Bool literal (eg. true)
-    TOK_FUN_NAME,   // Function name (eg. write)
-    TOK_DOT,        // .
-    TOK_COMMA,      // ,
-    TOK_COLON,      // :
-    TOK_SEMICOLON,  // ;
-    TOK_ASSIGN,     // =
-    TOK_EQUALS,     // ===
-    TOK_NEQUALS,    // !==
     TOK_PLUS,       // +
     TOK_MINUS,      // -
     TOK_MULTIPLY,   // *
     TOK_DIVIDE,     // /
     TOK_LESS,       // <
-    TOK_GREATER,    // >
     TOK_LESS_E,     // <=
+    TOK_GREATER,    // >
     TOK_GREATER_E,  // >=
+    TOK_EQUALS,     // ===
+    TOK_NEQUALS,    // !==
     TOK_LPAREN,     // (
     TOK_RPAREN,     // )
+    TOK_VAR,        // Identifier (eg. $foo)
+    TOK_INT_LIT,    // Integer literal (eg. 0)
+    TOK_FLOAT_LIT,  // Float literal (eg. 0.0)
+    TOK_STR_LIT,    // String literal (eg. "foo")
+    TOK_DOT,        // .
+    TOK_EOF,        // End of file
+    TOK_BOOL_LIT,   // Bool literal (eg. true)
+    TOK_FUN_NAME,   // Function name (eg. write)
+    TOK_COMMA,      // ,
+    TOK_COLON,      // :
+    TOK_SEMICOLON,  // ;
+    TOK_ASSIGN,     // =
     TOK_LBRACE,     // {
     TOK_RBRACE,     // }
 
